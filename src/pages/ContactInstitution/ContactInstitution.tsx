4b58c572e2aa726dd909e551e156a5185af898b6
Kev206 <kevinnoeter237@gmail.com>
2024-07-12 11:13:12 -0600

    Implementación de validaciones y errores

--- src/pages/ContactInstitution/ContactInstitution.tsx
+++ src/pages/ContactInstitution/ContactInstitution.tsx
@@ -1,42 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { IonButton, IonContent, IonInput, IonItem, IonList, IonPage, IonTextarea } from '@ionic/react';
+import { IonButton, IonContent, IonInput, IonItem, IonList, IonPage, IonTextarea, IonToast } from '@ionic/react';
 import NavbarHeader from '../../components/NavbarHeader/NavbarHeader';
+import { useForm } from 'react-hook-form';
 
 const ContactInstitution: React.FC = () => {
     const { id } = useParams<{ id: string }>();
+    
+    const {register, handleSubmit, formState: { errors }} = useForm();
+    
+    const [showToast, setShowToast] = useState<string>('');
+
+    const [toastMessage, setToastMessage] = useState('');
+
+    const handleContinue = () => {
+        console.log('Continuar...');
+    }
+
+
+    const onError = (error: any) => {
+        if (error.nombre) {
+            setToastMessage(error.nombre.message);
+            setShowToast('error');
+        } else if (error.apellidoMaterno) {
+            setToastMessage(error.apellidoMaterno.message);
+            setShowToast('error');
+        } else if (error.apellidoPaterno) {
+            setToastMessage(error.apellidoPaterno.message);
+            setShowToast('error');
+        } else if (error.telefono) {
+            setToastMessage(error.telefono.message);
+            setShowToast('error');
+        } else if (error.email) {
+            setToastMessage(error.email.message);
+            setShowToast('error');
+        } else if (error.mensaje) {
+            setToastMessage(error.mensaje.message);
+            setShowToast('error');
+        }
+    }
+
 
     return (
         <IonPage>
             <NavbarHeader title="Contactar" />
 
             <IonContent className='ion-padding'>
-                <p>¡Estas a un paso de reunirte con esa persona querida! Llena los campos con los datos requeridos para concluir tu solicitud.</p>
+                <form onSubmit={handleSubmit((data) => console.log(data))}>
+                    <p>¡Estas a un paso de reunirte con esa persona querida! Llena los campos con los datos requeridos para concluir tu solicitud.</p>
 
-                <h4>Tu información de contacto</h4>
-                <IonList>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Nombre(s)" labelPlacement="stacked" placeholder="Ingresa tu nombre"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Apellido materno" labelPlacement="stacked" placeholder="Ingresa tu apellido materno"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Apellido paterno" labelPlacement="stacked" placeholder="Ingresa tu apellido paterno"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Número de teléfono" labelPlacement="stacked" placeholder="Ej. 221-236-2345"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Correo electrónico (opcional)" labelPlacement="stacked" placeholder="correo@ejemplo.com"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonTextarea label="Mensaje (Opcional)" labelPlacement="stacked" placeholder="Algún mensaje o nota para la persona encargada de dar seguimiento a tu solicitud"></IonTextarea>
-                    </IonItem>
-                </IonList>
+                    <h4>Tu información de contacto</h4>
+                    <IonList>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonInput label="Nombre(s)" labelPlacement="stacked" placeholder="Ingresa tu nombre"
+                            {...register("nombre",{required:true, pattern: {value: /^[A-Za-z]+$/i, message: "Solo se aceptan letras"}})}></IonInput>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonInput label="Apellido materno" labelPlacement="stacked" placeholder="Ingresa tu apellido materno"
+                            {...register("apellidoMaterno",{required:true, pattern: {value: /^[A-Za-z]+$/i, message: "Solo se aceptan letras"}})}></IonInput>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonInput label="Apellido paterno" labelPlacement="stacked" placeholder="Ingresa tu apellido paterno"
+                            {...register("apellidoPaterno",{required:true, pattern: {value: /^[A-Za-z]+$/i, message: "Solo se aceptan letras"}})}></IonInput>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonInput label="Número de teléfono" labelPlacement="stacked" placeholder="Ej. 221-236-2345"
+                            {...register("tel",{required:true, pattern: {value: /^[0-9]+$/i, message: "Solo se aceptan numeros"}, maxLength: {value: 10, message: "Solo se aceptan 10 digitos"}, minLength: {value: 10, message: "Solo se aceptan 10 digitos"}})}></IonInput>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonInput label="Correo electrónico (opcional)" labelPlacement="stacked" placeholder="correo@ejemplo.com"
+                            {...register("email",{pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: "Ingresa un correo valido"}})}></IonInput>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonTextarea label="Mensaje (Opcional)" labelPlacement="stacked" placeholder="Algún mensaje o nota para la persona encargada de dar seguimiento a tu solicitud"></IonTextarea>
+                        </IonItem>
+                    </IonList>
 
-                <IonButton className='ion-margin-top' expand="block" mode='ios' routerLink={`/process-completed/${id}`}>Contactar</IonButton>
+
+                    <IonButton className='ion-margin-top' expand="block" mode='ios' routerLink={`/process-completed/${id}`}>Contactar</IonButton>
+                </form>
+
+                <IonToast
+                    isOpen={showToast === 'error'}
+                    onDidDismiss={() => setShowToast('')}
+                    message={toastMessage}
+                    duration={2000}
+                ></IonToast>
+
             </IonContent>
+
         </IonPage>
     );
 };
--- src/pages/InputPatientInformation/InputPatientInformation.tsx
+++ src/pages/InputPatientInformation/InputPatientInformation.tsx
@@ -1,73 +1,192 @@
 import { IonButton, IonContent, IonInput, IonItem, IonList, IonPage, IonSelect, IonSelectOption } from '@ionic/react';
 import NavbarHeader from '../../components/NavbarHeader/NavbarHeader';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const InputPatientInformation: React.FC = () => {
+
+    
+
+    const {register,formState: {errors}, handleSubmit} = useForm();
+
+    const [showToast, setShowToast] = useState<string>('');
+
+    const [toastMessage, setToastMessage] = useState('');
+
+    const onError = (error: any) => {
+        if (error.nombre) {
+            setToastMessage(error.nombre.message);
+            setShowToast('error');
+        } else if (error.apellidoMaterno) {
+            setToastMessage(error.apellidoMaterno.message);
+            setShowToast('error');
+        } else if (error.apellidoPaterno) {
+            setToastMessage(error.apellidoPaterno.message);
+            setShowToast('error');
+        } else if (error.nss) {
+            setToastMessage(error.nss.message);
+            setShowToast('error');
+        } else if (error.postura) {
+            setToastMessage(error.postura.message);
+            setShowToast('error');
+        } else if (error.descripcion) {
+            setToastMessage(error.descripcion.message);
+            setShowToast('error');
+        } else if (error.condicionesEspeciales) {
+            setToastMessage(error.condicionesEspeciales.message);
+            setShowToast('error');
+        }
+    };
+
+
     return (
         <IonPage>
             <NavbarHeader title="Un paso más..." />
 
             <IonContent className='ion-padding'>
-                <p>Llena el siguiente formulario para mejores coincidencias. Tu ayuda es fundamental.</p>
+                
+            <form onSubmit={handleSubmit((data) => console.log(data))}>
+                    <p>Llena el siguiente formulario para mejores coincidencias. Tu ayuda es fundamental.</p>
 
-                <h4>Información principal</h4>
-                <IonList>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Nombre(s)" labelPlacement="stacked" placeholder="Ingresa el nombre del paciente"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Apellido materno" labelPlacement="stacked" placeholder="Ingresa el apellido materno del paciente"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Apellido paterno" labelPlacement="stacked" placeholder="Ingresa el apellido paterno del paciente"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonSelect label="Tipo de sangre" labelPlacement="stacked" placeholder="Selecciona el tipo de sangre">
-                            <IonSelectOption value="apple">A+</IonSelectOption>
-                            <IonSelectOption value="banana">O-</IonSelectOption>
-                            <IonSelectOption value="orange">A-</IonSelectOption>
-                        </IonSelect>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonSelect label="Nacionalidad" labelPlacement="stacked" placeholder="Selecciona la nacionalidad">
-                            <IonSelectOption value="apple">Mexicana</IonSelectOption>
-                            <IonSelectOption value="banana">Hondureña</IonSelectOption>
-                            <IonSelectOption value="orange">Estadounidense</IonSelectOption>
-                        </IonSelect>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Número de seguridad social (NSS)" labelPlacement="stacked" placeholder="Ingresa el NSS"></IonInput>
-                    </IonItem>
-                </IonList>
+                    <h4>Información principal</h4>
+                    <IonList>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonInput label="Nombre(s)" labelPlacement="stacked" placeholder="Ingresa el nombre del paciente"
+                            {...register("nombre",{required:true, pattern: {value: /^[A-Za-z]+$/i, message: "Solo se aceptan letras"}})}></IonInput>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonInput label="Apellido materno" labelPlacement="stacked" placeholder="Ingresa el apellido materno del paciente"
+                            {...register("apellidoMaterno",{required:true, pattern: {value: /^[A-Za-z]+$/i, message: "Solo se aceptan letras"}})}></IonInput>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonInput label="Apellido paterno" labelPlacement="stacked" placeholder="Ingresa el apellido paterno del paciente"
+                            {...register("ApellidoPaterno",{required:true, pattern: {value: /^[A-Za-z]+$/i, message: "Solo se aceptan letras"}})}></IonInput>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonSelect label="Tipo de sangre" labelPlacement="stacked" placeholder="Selecciona el tipo de sangre">
+                                <IonSelectOption value="a+">A+</IonSelectOption>
+                                <IonSelectOption value="a-">A-</IonSelectOption>
+                                <IonSelectOption value="b+">B+</IonSelectOption>
+                                <IonSelectOption value="b-">B-</IonSelectOption>
+                                <IonSelectOption value="ab+">AB+</IonSelectOption>
+                                <IonSelectOption value="ab-">AB-</IonSelectOption>
+                                <IonSelectOption value="o+">O-</IonSelectOption>
+                                <IonSelectOption value="o-">O-</IonSelectOption>
+                            </IonSelect>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonSelect label="Nacionalidad" labelPlacement="stacked" placeholder="Selecciona la nacionalidad">
+                                <IonSelectOption value="mexicana">Mexicana</IonSelectOption>
+                                <IonSelectOption value="hondurena">Hondureña</IonSelectOption>
+                                <IonSelectOption value="estadounidense">Estadounidense</IonSelectOption>
+                                <IonSelectOption value="brasilena">Brasileña</IonSelectOption>
+                                <IonSelectOption value="argentina">Argentina</IonSelectOption>
+                                <IonSelectOption value="colombiana">Colombiana</IonSelectOption>
+                                <IonSelectOption value="venezolana">Venezolana</IonSelectOption>
+                                <IonSelectOption value="cubana">Cubana</IonSelectOption>
+                                <IonSelectOption value="peruana">Peruana</IonSelectOption>
+                                <IonSelectOption value="chilena">Chilena</IonSelectOption>
+                                <IonSelectOption value="Otros">Otros</IonSelectOption>
+                            </IonSelect>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonInput label="Número de seguridad social (NSS)" labelPlacement="stacked" placeholder="Ingresa el NSS"
+                            {...register("NSS",{required:true, pattern: {value: /^[0-9]{11}$/, message: "El NSS debe contener 11 digitos"}})}></IonInput>
+                        </IonItem>
+                    </IonList>
 
-                <h4>Información morfológica</h4>
-                <IonList>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Input número uno" labelPlacement="stacked" placeholder="Ingresa el input número uno"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Input número dos" labelPlacement="stacked" placeholder="Ingresa el input número dos"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Input número tres" labelPlacement="stacked" placeholder="Ingresa el input número tres"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Input número cuatro" labelPlacement="stacked" placeholder="Ingresa el input número cuatro"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Input número cinco" labelPlacement="stacked" placeholder="Ingresa el input número cinco"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Input número seis" labelPlacement="stacked" placeholder="Ingresa el input número seis"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Input número siete" labelPlacement="stacked" placeholder="Ingresa el input número siete"></IonInput>
-                    </IonItem>
-                    <IonItem className='ion-margin-bottom'>
-                        <IonInput label="Input número ocho" labelPlacement="stacked" placeholder="Ingresa el input número ocho"></IonInput>
-                    </IonItem>
-                </IonList>
+                    <h4>Información morfológica</h4>
+                    <IonList>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonSelect label="Color de cabello" labelPlacement="stacked" placeholder="Selecciona un color de cabello">
+                                <IonSelectOption value="blanco">Blanco</IonSelectOption>
+                                <IonSelectOption value="rubio">Rubio</IonSelectOption>
+                                <IonSelectOption value="rubio oscuro">Rubio oscuro</IonSelectOption>
+                                <IonSelectOption value="rojo">Rojo</IonSelectOption>
+                                <IonSelectOption value="marron rojizo">Marrón rojizo</IonSelectOption>
+                                <IonSelectOption value="marron claro">Marrón claro</IonSelectOption>
+                                <IonSelectOption value="marron">Marrón</IonSelectOption>
+                                <IonSelectOption value="marron oscuro">Marrón oscuro</IonSelectOption>
+                                <IonSelectOption value="negro">Negro</IonSelectOption>
+                                <IonSelectOption value="otros">Otros</IonSelectOption>
+                            </IonSelect>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonSelect label="Tipo de cabello" labelPlacement="stacked" placeholder="Selecciona un tipo de cabello">
+                                <IonSelectOption value="liso">Liso</IonSelectOption>
+                                <IonSelectOption value="ondulado">Ondulado</IonSelectOption>
+                                <IonSelectOption value="rizado">Rizado</IonSelectOption>
+                                <IonSelectOption value="afro">Muy rizado/Afro</IonSelectOption>
+                                <IonSelectOption value="otros">Otros</IonSelectOption>
+                            </IonSelect>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonSelect label="Color de piel" labelPlacement="stacked" placeholder="Selecciona un color de piel">
+                                <IonSelectOption value="muyClara">Piel muy clara</IonSelectOption>
+                                <IonSelectOption value="clara">Piel clara</IonSelectOption>
+                                <IonSelectOption value="mediaClara">Piel media clara</IonSelectOption>
+                                <IonSelectOption value="media">Piel media</IonSelectOption>
+                                <IonSelectOption value="mediaOscura">Piel media oscura</IonSelectOption>
+                                <IonSelectOption value="oscura">Piel oscura</IonSelectOption>
+                                <IonSelectOption value="muyOscura">Piel muy oscura</IonSelectOption>
+                                <IonSelectOption value="otros">Otros</IonSelectOption>
+                            </IonSelect>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonSelect label="Color de ojos" labelPlacement="stacked" placeholder="Selecciona un color de ojos">
+                                <IonSelectOption value="ambar">Ámbar</IonSelectOption>
+                                <IonSelectOption value="castano">Castaño</IonSelectOption>
+                                <IonSelectOption value="gris">Gris</IonSelectOption>
+                                <IonSelectOption value="azul">Azul</IonSelectOption>
+                                <IonSelectOption value="verde">Verde</IonSelectOption>
+                                <IonSelectOption value="avellana">Avellana</IonSelectOption>
+                                <IonSelectOption value="negro">Negro</IonSelectOption>
+                                <IonSelectOption value="violeta">Violeta</IonSelectOption>
+                                <IonSelectOption value="rojo">Rojo</IonSelectOption>
+                                <IonSelectOption value="otros">Otros</IonSelectOption>
+                            </IonSelect>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonSelect label="Género" labelPlacement="stacked" placeholder="Selecciona un género">
+                                <IonSelectOption value="masculino">Masculino</IonSelectOption>
+                                <IonSelectOption value="femenino">Femenino</IonSelectOption>
+                                <IonSelectOption value="noBinario">No binario</IonSelectOption>
+                                <IonSelectOption value="noDecir">Prefiero no decirlo</IonSelectOption>
+                                <IonSelectOption value="otro">Otro</IonSelectOption>
+                            </IonSelect>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonSelect label="Complexión" labelPlacement="stacked" placeholder="Selecciona una complexion">
+                                <IonSelectOption value="esbelto">Esbelto</IonSelectOption>
+                                <IonSelectOption value="corpulento">Corpulento</IonSelectOption>
+                                <IonSelectOption value="musculoso">Musculoso</IonSelectOption>
+                                <IonSelectOption value="fornido">Fornido</IonSelectOption>
+                                <IonSelectOption value="fibrado">Fibrado</IonSelectOption>
+                                <IonSelectOption value="gordo">Gordo</IonSelectOption>
+                                <IonSelectOption value="delgaducho">Delgaducho</IonSelectOption>
+                                <IonSelectOption value="vigoroso">Vigoroso</IonSelectOption>
+                                <IonSelectOption value="enjuto">Enjuto</IonSelectOption>
+                                <IonSelectOption value="espaldaAncha">Ancho de espalda</IonSelectOption>
+                                <IonSelectOption value="otra">Otra</IonSelectOption>
+                            </IonSelect>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonInput label="Postura" labelPlacement="stacked" placeholder="Ingresa la postura del paciente"
+                            {...register("postura",{required:true, pattern: {value: /^[A-Za-z]+$/i, message: "Solo se aceptan letras"}})}></IonInput>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonInput label="Descripción" labelPlacement="stacked" placeholder="Ingrese una descripción detallada"
+                            {...register("descripcion",{required:true, pattern: {value: /^[A-Za-z]+$/i, message: "Solo se aceptan letras"}})}></IonInput>
+                        </IonItem>
+                        <IonItem className='ion-margin-bottom'>
+                            <IonInput label="Condiciones especiales (si aplica)" labelPlacement="stacked" placeholder="Discapacidad, amputaciones, etc."
+                            {...register("condicionesEspeciales",{required:true, pattern: {value: /^[A-Za-z]+$/i, message: "Solo se aceptan letras"}})}></IonInput>
+                        </IonItem>
+                    </IonList>
 
-                <IonButton className='ion-margin-top' expand="block" mode='ios' routerLink='/loading-images'>Continuar</IonButton>
+                    <IonButton className='ion-margin-top' expand="block" mode='ios' routerLink='/loading-images'>Continuar</IonButton>
+
+                </form>
 
             </IonContent>
         </IonPage>
